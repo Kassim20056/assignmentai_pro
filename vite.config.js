c79a4a49c9146ad08b95d40c0a31f08d8ba59739
Kassim20056 <kassimgaleb@gmail.com>
2025-07-24 00:32:19 +0800

    Add explicit aliases to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
     viteTsconfigPaths()
   ],
+  resolve: {
+    alias: {
+      'components': path.resolve(__dirname, './src/components'),
+      'pages': path.resolve(__dirname, './src/pages'),
+      'utils': path.resolve(__dirname, './src/utils'),
+      'styles': path.resolve(__dirname, './src/styles')
+    }
+  },
   server: {
     port: 3000,
     open: true
